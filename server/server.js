67ab1b8e39b216000c632cd20837e087b9ca7ffd
Ben <bensims89@live.com>
2019-09-06 16:53:11 -0700

    Adds API call to server.js

--- server/server.js
+++ server/server.js
@@ -7,4 +7,24 @@ app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
 
+app.get('/movieInfo/:inputValue', (req, res) => {
+    axios({
+      url: `http://omdbapi.com/?s=${req.params.inputValue}&apikey=${process.env.OMDB_API_KEY}`,
+      method: 'get'
+    })
+    .then((response) => {
+      res.send(response.data);
+    });
+  });
+  
+app.get('/movie/:id', (req, res) => {
+    axios({
+        url: `http://omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDB_API_KEY}`,
+        method: 'get'
+    })
+    .then((response) => {
+        res.send(response.data);
+    });
+});
+
 module.exports = app;
