f842bff3b057218a8fb58990db11fecdd464a209
srujansurapaneni <srujansurapaneni@gmail.com>
2020-02-09 20:52:55 -0500

    fixed axios

--- server/server.js
+++ server/server.js
@@ -1,32 +1,30 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const morgan = require('morgan');
-
+const express = require("express");
+const morgan = require("morgan");
+const axios = require("axios");
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static('dist'));
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("dist"));
+app.use(express.static("public"));
 
-app.get('/movieInfo/:inputValue', (req, res) => {
-    axios({
-      url: `http://omdbapi.com/?s=${req.params.inputValue}&apikey=${process.env.OMDB_API_KEY}`,
-      method: 'get'
-    })
-    .then((response) => {
-      res.send(response.data);
-    });
+app.get("/movieInfo/:inputValue", (req, res) => {
+  axios({
+    url: `http://omdbapi.com/?s=${req.params.inputValue}&apikey=${process.env.OMDB_API_KEY}`,
+    method: "get"
+  }).then(response => {
+    res.send(response.data);
   });
+});
 
-app.get('/movie/:id', (req, res) => {
-    axios({
-        url: `http://omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDB_API_KEY}`,
-        method: 'get'
-    })
-    .then((response) => {
-        res.send(response.data);
-    });
+app.get("/movie/:id", (req, res) => {
+  axios({
+    url: `http://omdbapi.com/?i=${req.params.id}&apikey=${process.env.OMDB_API_KEY}`,
+    method: "get"
+  }).then(response => {
+    res.send(response.data);
+  });
 });
 
 module.exports = app;
